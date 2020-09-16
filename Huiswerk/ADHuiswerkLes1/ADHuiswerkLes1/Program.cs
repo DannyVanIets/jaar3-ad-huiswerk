19a887e7dfc8f47499ab128a5323c70e32efb34d
DannyVanIets <dannyvanbokhorst@live.nl>
2020-09-16 19:56:13 +0200

    Les 1 huiswerk en les 3 toegevoegd hieraan

---
+++ Huiswerk/ADHuiswerkLes1/ADHuiswerkLes1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ADHuiswerkLes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int sum = 0;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Fragment 7
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n * n; j++)
+                    for (int k = 0; k < j; k++)
+                        sum++;
+
+            stopwatch.Stop();
+            Console.WriteLine("Sum: " + sum.ToString() + ". Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+}
---
+++ Lessen/les3/les3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace les3
+{
+    public class Program
+    {
+        static public int Sum(int n) //Hiervoor gebruiken we mod en div. Laatste getal is met mod. De rest is div.
+        {
+            if(n < 0) // Voor mingetallen.
+            {
+                return Sum(-n);
+            }
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Sum(n / 10) + n % 10;
+            }
+        }
+        // Hallo -> ollah.
+        static public string Reverse(string s) // Probeer niet hierin een console.writeline neerzet! Mag wel voor debuggen, maar niet in het tentamen!
+        {
+            if (s.Length == 0) // Hiermee hou je rekening voor een lege string.
+            {
+                return "";
+            }
+            else
+            {
+                return Reverse(s.Substring(1)) + s[0];
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(123));
+            Console.WriteLine(Sum(2525));
+            Console.WriteLine(Reverse("Hallo"));
+            Console.WriteLine(Reverse("Hallo"));
+        }
+    }
+}

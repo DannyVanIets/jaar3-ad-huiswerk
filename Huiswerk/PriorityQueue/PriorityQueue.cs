8659f72c5f0ed743b29c9a53eba2a76fec28b21c
DannyVanIets <dannyvanbokhorst@live.nl>
2020-09-28 16:15:20 +0200

    Alles van opdracht 2 gedaan!

--- Huiswerk/PriorityQueue/PriorityQueue.cs
+++ Huiswerk/PriorityQueue/PriorityQueue.cs
@@ -42,11 +42,18 @@ namespace AD
             }
             else
             {
-                if (size + 1 == array.Length)
+                DoubleArrayLength();
+                // Check if the new value being added is smaller than the parent.
+                // If so, then we will swap the parent with the newly added value.
+                // Which is called PercolateUp!
+                if (x.CompareTo(array[size / 2]) < 0)
                 {
-                    // Double the array size here!
+                    PercolateUp(x);
                 }
-                array[size] = x;
+                else
+                {
+                    array[size] = x;
+                }
             }
         }
 
@@ -57,10 +64,18 @@ namespace AD
             {
                 throw new PriorityQueueEmptyException();
             }
+            else if (size == 1)
+            {
+                array = new T[DEFAULT_CAPACITY];
+                return default;
+            }
             else
             {
+                array[1] = array[size];
+                array[size] = default;
                 size--;
-                return default;
+                PercolateDown(1);
+                return array[1];
             }
         }
 
@@ -85,16 +100,74 @@ namespace AD
             return everythingInTheArray;
         }
 
+        public void DoubleArrayLength()
+        {
+            if (size + 1 >= array.Length)
+            {
+                T[] temporaryArray = array;
+                array = new T[size * 2];
+
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = temporaryArray[i];
+                }
+            }
+        }
+
+        private void PercolateUp(T x)
+        {
+            // In tempValue we will store the parent temporary.
+            T tempValue;
+
+            // In parentPlace we will store the placement of the current parent of the newly added node.
+            // In parentPlace will be updated at the end of the while loop,
+            // after the newly added node has been swapped with the older parent.
+            int parentPlace = size / 2;
+
+            // In swapWithNewNodePlacement we will store the placement of the newly added
+            // node and we will update it after it has been swapped.
+            int swapWithNewNodePlacement = size;
+
+            while (x.CompareTo(array[parentPlace]) < 0)
+            {
+                tempValue = array[parentPlace];
+                array[parentPlace] = x;
+                array[swapWithNewNodePlacement] = tempValue;
+
+                swapWithNewNodePlacement = parentPlace;
+                parentPlace /= 2;
+            }
+        }
+
         private void PercolateDown(int node)
         {
+            T tempValue = array[node];
+            int leftChild = node * 2;
+            int rightChild = node * 2 + 1;
 
+            if (leftChild < size || rightChild < size)
+            {
+                if (array[leftChild].CompareTo(array[rightChild]) < 0)
+                {
+                    array[node] = array[leftChild];
+                    array[leftChild] = tempValue;
+
+                    PercolateDown(leftChild);
+                }
+                else
+                {
+                    array[node] = array[rightChild];
+                    array[rightChild] = tempValue;
+
+                    PercolateDown(rightChild);
+                }
+            }
+            else if (size == 2)
+            {
+                array[node] = array[leftChild];
+                array[leftChild] = tempValue;
+            }
         }
-
-        private void PercolateUp(int node)
-        {
-
-        }
-
 
         //----------------------------------------------------------------------
         // Interface methods that have to be implemented for homework

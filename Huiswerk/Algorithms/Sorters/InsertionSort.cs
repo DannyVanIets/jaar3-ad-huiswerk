c7a3a133383f703cc62aa9f322c236c40fa44fc3
DannyVanIets <dannyvanbokhorst@live.nl>
2020-10-05 13:50:47 +0200

    Restructure

---
+++ Huiswerk/Algorithms/Sorters/InsertionSort.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+
+namespace AD
+{
+    public partial class InsertionSort : Sorter
+    {
+        public override void Sort(List<int> list)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

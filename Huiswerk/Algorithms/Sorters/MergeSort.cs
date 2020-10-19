15a0a314d59283c59ab62b6050b281a946f7961e
DannyVanIets <dannyvanbokhorst@live.nl>
2020-10-19 20:18:53 +0200

    Mergesort verder afgewerkt

--- Huiswerk/Algorithms/Sorters/MergeSort.cs
+++ Huiswerk/Algorithms/Sorters/MergeSort.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 
@@ -7,14 +8,50 @@ namespace AD
     {
         public void Merge(List<int> list, int left, int center, int right)
         {
+            List<int> leftArray = new List<int>();
+            List<int> rightArray = new List<int>();
 
+            for (int l = 0; l < center; l++)
+            {
+                leftArray.Add(list[l]);
+            }
 
-            List<int> leftArray = list[left, center];
+            for (int r = center; r < list.Count; r++)
+            {
+                rightArray.Add(list[r]);
+            }
+
+            int i = 0;
+            int j = 0;
+
+            for (int m = left; m < right + 1; m++)
+            {
+                if (i == leftArray.Count)
+                {
+                    list[m] = rightArray[j];
+                    j++;
+                }
+                else if (j == rightArray.Count)
+                {
+                    list[m] = leftArray[i];
+                    i++;
+                }
+                else if (leftArray[i] <= rightArray[j])
+                {
+                    list[m] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    list[m] = rightArray[j];
+                    j++;
+                }
+            }
         }
 
         public void Mergesort(List<int> list, int left, int right)
         {
-            if(left < right)
+            if (left < right)
             {
                 int center = (left + right) / 2;
 
@@ -26,7 +63,7 @@ namespace AD
 
         public override void Sort(List<int> list)
         {
-            if(list.Count > 0)
+            if (list.Count > 0)
             {
                 Mergesort(list, 0, list.Count - 1);
             }
